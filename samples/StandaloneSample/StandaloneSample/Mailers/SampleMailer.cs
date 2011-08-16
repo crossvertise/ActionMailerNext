b24cd64ad3be56155a725185f85977a5814854a8
Scott Anderson <scott@geeksharp.com>
2011-08-16 14:19:54 -0500

    Adding in a sample project, however it is not complete.

---
+++ samples/StandaloneSample/StandaloneSample/Mailers/SampleMailer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ActionMailer.Net.Standalone;
+
+namespace StandaloneSample.Mailers {
+    public class SampleMailer : RazorMailerBase {
+        private readonly string _viewPath;
+        public override string ViewPath {
+            get { return _viewPath; }
+        }
+
+        public SampleMailer() {
+            var workingDirectory = Assembly.GetExecutingAssembly().Location;
+            _viewPath = Path.Combine(workingDirectory, "..", "..", "Templates");
+        }
+
+        public RazorEmailResult EmailWithNoModel() {
+            
+        }
+
+        
+    }
+}
