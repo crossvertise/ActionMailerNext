52ca38446a9448e7debe19fe723586ce09d394f7
Scott Anderson <scott@geeksharp.com>
2012-08-05 19:33:48 -0500

    Fixing build script

--- create-new-release.bat
+++ create-new-release.bat
@@ -12,14 +12,14 @@ SET NgStandaloneDir=%CD%\nuget\base\ActionMailer.Standalone
 
 ECHO Performing clean release build...
 
-CMD /c C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %CD%\src\ActionMailer.Net.sln /t:Clean /p:Configuration=Release
-CMD /c C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %CD%\src\ActionMailer.Net.sln /t:Rebuild /p:Configuration=Release
+CMD /c %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %CD%\src\ActionMailer.Net.sln /t:Clean /p:Configuration=Release
+CMD /c %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe %CD%\src\ActionMailer.Net.sln /t:Rebuild /p:Configuration=Release
 
 ECHO Compressing ActionMailer.Net.Mvc to Zip...
 
 CMD /c "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip %MvcDir%\ActionMailer.Net.Mvc-v%NewVersion%.zip %MvcDir%\*.dll %MvcDir%\*.pdb %MvcDir%\*.txt %MvcDir%\*.xml
-CMD /c "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip %PostmarkDir%\ActionMailer.Net.Mvc-v%NewVersion%.zip %PostmarkDir%\*.dll %PostmarkDir%\*.pdb %PostmarkDir%\*.txt %PostmarkDir%\*.xml
-CMD /c "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip %StandaloneDir%\ActionMailer.Net.Mvc-v%NewVersion%.zip %StandaloneDir%\*.dll %StandaloneDir%\*.pdb %StandaloneDir%\*.txt %StandaloneDir%\*.xml
+CMD /c "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip %PostmarkDir%\ActionMailer.Net.Postmark-v%NewVersion%.zip %PostmarkDir%\*.dll %PostmarkDir%\*.pdb %PostmarkDir%\*.txt %PostmarkDir%\*.xml
+CMD /c "%PROGRAMFILES%\7-Zip\7z.exe" a -tzip %StandaloneDir%\ActionMailer.Net.Standalone-v%NewVersion%.zip %StandaloneDir%\*.dll %StandaloneDir%\*.pdb %StandaloneDir%\*.txt %StandaloneDir%\*.xml
 
 ECHO Creating NuGet Packages...
 
