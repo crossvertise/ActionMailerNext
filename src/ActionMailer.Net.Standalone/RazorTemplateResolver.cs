7bf23bc9936e80708dce15b011c16cc9d9057eaf
Scott Anderson <scott@geeksharp.com>
2013-03-30 10:53:00 -0500

    Some more minor cleanup.

--- src/ActionMailer.Net.Standalone/RazorTemplateResolver.cs
+++ src/ActionMailer.Net.Standalone/RazorTemplateResolver.cs
@@ -1,4 +1,5 @@
-﻿using RazorEngine.Templating;
+﻿using System.Collections.Generic;
+using RazorEngine.Templating;
 using System;
 using System.IO;
 
@@ -10,10 +11,20 @@ namespace ActionMailer.Net.Standalone {
     public class RazorTemplateResolver : ITemplateResolver {
         private readonly string _viewPath;
 
+        /// <summary>
+        /// Creates a template resolver using the specified path.  If no path is given, this defaults to "Views"
+        /// </summary>
+        /// <param name="viewPath">The path containing your views</param>
         public RazorTemplateResolver(string viewPath) {
             _viewPath = viewPath ?? "Views";
         }
 
+        /// <summary>
+        /// Searches the view path for the given template and returns the contents of the view.
+        /// Throws a TemplateResolvingException if no views could be found.
+        /// </summary>
+        /// <param name="name">The name of the view to search for</param>
+        /// <returns>The contents of any views found.</returns>
         public string Resolve(string name) {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentNullException("name");
@@ -38,7 +49,7 @@ namespace ActionMailer.Net.Standalone {
             if (File.Exists(vbViewPath))
                 return File.ReadAllText(vbViewPath);
 
-            throw new TemplateResolvingException();
+            throw new TemplateResolvingException {SearchPaths = new List<string> {csViewPath, vbViewPath}};
         }
     }
 }
--- src/ActionMailer.Net.Standalone/TemplateResolvingException.cs
+++ src/ActionMailer.Net.Standalone/TemplateResolvingException.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 
 namespace ActionMailer.Net.Standalone {
+    /// <summary>
+    /// Exception thrown when a template could not be resolved
+    /// </summary>
     public class TemplateResolvingException : Exception {
+        /// <summary>
+        /// A list of paths that were checked when searching for a template.
+        /// </summary>
         public List<string> SearchPaths { get; set; }
     }
 }
