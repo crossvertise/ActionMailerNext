0adb29e5a315a106f4f23680e29bfc2619459be8
Andrew Kamel <a.kamel@crossvertise.com>
2014-09-12 13:46:07 +0200

    Pumped up to version 2.1.0.0

--- src/ActionMailerNext/Properties/AssemblyInfo.cs
+++ src/ActionMailerNext/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("6125d9cc-3dec-454c-82ed-c57bf6f0d482")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
