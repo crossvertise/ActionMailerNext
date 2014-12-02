c2cd67d01df50384e7e50883a6edbf09f8f0e2f3
Andrew Kamel <a.kamel@crossvertise.com>
2014-12-02 19:34:03 +0100

    Pump the version up

--- src/ActionMailerNext.Standalone/Properties/AssemblyInfo.cs
+++ src/ActionMailerNext.Standalone/Properties/AssemblyInfo.cs
@@ -38,6 +38,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyInformationalVersion("3.0.0-rc")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.1.0-rc")]
+[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.1.0")]
