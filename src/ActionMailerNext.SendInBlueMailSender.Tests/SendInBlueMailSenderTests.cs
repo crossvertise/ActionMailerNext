e5ef6fcb4b0d9e138979d5d6bd0c38d0eb4664ae
mohammadniazmand <104848846+mohammadniazmand@users.noreply.github.com>
2022-06-14 11:53:39 +0200

    [MAR-12652] Write tests for SendInBlueMailSender

---
+++ src/ActionMailerNext.SendInBlueMailSender.Tests/SendInBlueMailSenderTests.cs
@@ -0,0 +1,55 @@
+﻿namespace ActionMailerNext.MandrillMailSender.Tests
+{
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Net.Mail;
+    using System.Net.Mime;
+
+    [TestFixture]
+    public class SendInBlueMailSenderTests
+    {
+        private readonly SendInBlueMailSender _sendInBlueMailSender;
+        public SendInBlueMailSenderTests()
+        {
+            _sendInBlueMailSender = new SendInBlueMailSender("SendInBlue Api-Key", null);
+        }
+
+        [Test]
+        public void Send_GivenCorrectMail_ReturnResponseWithQueuedStatus()
+        {
+            //Act
+            var response = _sendInBlueMailSender.Send(
+                 new MailAttributes()
+                 {
+                     To = new List<MailAddress>() { new MailAddress("Email Address") },
+                     Subject = "SendInBlue Test",
+                     Body = "test body",
+                     TextBody = "Test sending email with SendInBlue from ActionMailer project",
+                     AlternateViews = new List<AlternateView>() { new AlternateView("content.txt", MediaTypeNames.Text.Plain) },
+                     From = new MailAddress("Email Address", "name"),
+                     Tags = new List<string> { "Test" }
+                 });
+
+            //Assert
+            Assert.AreEqual(response[0].DeliveryStatus, DeliveryStatus.QUEUED);
+        }
+
+        [Test]
+        public void GenerateProspectiveMailMessage_GivenMailAttributesAreNotFilled_CorrespondFieldsShouldBeNull()
+        {
+            //Arrange
+            var mailAttribute = new MailAttributes { From = new MailAddress("sample@crossvertise.com") };
+
+            //Act
+            var result = _sendInBlueMailSender.GenerateProspectiveMailMessage(mailAttribute);
+
+            //Assert
+            Assert.IsNull(result.Attachment);
+            Assert.IsNull(result.Tags);
+            Assert.IsNull(result.Bcc);
+            Assert.IsNull(result.Cc);
+            Assert.IsNull(result.Headers);
+
+        }
+    }
+}
